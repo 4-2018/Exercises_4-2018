2ffb613c3574f86aa208960ed31fc93354e5dde1
jocololika1999@gmail.com <jocololika1999@gmail.com>
2020-11-08 16:15:23 +0100

    Konekcija i prikaz 	modified:   Exercises_4-2018/Exercises_4-2018/ExerciseResult.cs 	modified:   Exercises_4-2018/Exercises_4-2018/Exercises_4-2018.csproj 	modified:   Exercises_4-2018/Exercises_4-2018/Form1.Designer.cs 	modified:   Exercises_4-2018/Exercises_4-2018/Form1.cs 	modified:   Exercises_4-2018/Exercises_4-2018/Form1.resx 	new file:   Exercises_4-2018/Exercises_4-2018/bin/Debug/netcoreapp3.1/Exercises_4-2018.deps.json 	new file:   Exercises_4-2018/Exercises_4-2018/bin/Debug/netcoreapp3.1/Exercises_4-2018.runtimeconfig.dev.json 	new file:   Exercises_4-2018/Exercises_4-2018/bin/Debug/netcoreapp3.1/Exercises_4-2018.runtimeconfig.json 	new file:   Exercises_4-2018/Exercises_4-2018/obj/Debug/netcoreapp3.1/Exercises_4-2018.csproj.CopyComplete 	new file:   Exercises_4-2018/Exercises_4-2018/obj/Debug/netcoreapp3.1/Exercises_4-2018.csproj.FileListAbsolute.txt 	modified:   Exercises_4-2018/Exercises_4-2018/obj/Debug/netcoreapp3.1/Exercises_4-2018.designer.deps.json 	new file:   Exercises_4-2018/Exercises_4-2018/obj/Debug/netcoreapp3.1/Exercises_4_2018.Form1.resources 	modified:   Exercises_4-2018/Exercises_4-2018/obj/Exercises_4-2018.csproj.nuget.dgspec.json 	modified:   Exercises_4-2018/Exercises_4-2018/obj/project.assets.json

--- Exercises_4-2018/Exercises_4-2018/ExerciseResult.cs
+++ Exercises_4-2018/Exercises_4-2018/ExerciseResult.cs
@@ -11,6 +11,7 @@ namespace Exercises_4_2018
         public string StudentIndex { get; set; }
         public int Points { get; set; }
 
+        
         public ExerciseResult(int Id, string StudentName, string StudentIndex, int Points)
         {
             this.Id = Id;
@@ -21,7 +22,7 @@ namespace Exercises_4_2018
 
         public override string ToString()
         {
-            return "ID: " + this.Id + "\n\t" +
+            return "ID:" + this.Id + "\n\t" +
                    "Ime:" + this.StudentName + "\n\t" +
                    "Indeks:" + this.StudentIndex + "\n\t" +
                    "Poeni:" + this.Points + "\n\t";
