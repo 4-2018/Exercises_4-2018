7b989df1eafa00722a83e19a61b12107c8d96211
jocololika1999@gmail.com <jocololika1999@gmail.com>
2020-11-08 15:56:12 +0100

    Kreirana model klasa 	new file:   Exercises_4-2018/Exercises_4-2018/ExerciseResult.cs

---
+++ Exercises_4-2018/Exercises_4-2018/ExerciseResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_4_2018
+{
+    class ExerciseResult
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+
+        public ExerciseResult(int Id, string StudentName, string StudentIndex, int Points)
+        {
+            this.Id = Id;
+            this.StudentName = StudentName;
+            this.StudentIndex = StudentIndex;
+            this.Points = Points;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + this.Id + "\n\t" +
+                   "Ime:" + this.StudentName + "\n\t" +
+                   "Indeks:" + this.StudentIndex + "\n\t" +
+                   "Poeni:" + this.Points + "\n\t";
+        }
+
+    }
+}
